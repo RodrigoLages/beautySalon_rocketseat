b242bcf57a220ac92bdf822eb9503f4f428502bb
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-21 13:23:00 -0300

    Beauty Salon

---
+++ main.js
@@ -0,0 +1,30 @@
+//toggle menu icon
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+//toggle menu item
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+//add header shadow on scroll
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
