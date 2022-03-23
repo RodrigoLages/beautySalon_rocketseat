e1f01a504eb54bb1ad06983b271b0921f7b2d568
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-23 12:57:57 -0300

    Added a back to top button

--- main.js
+++ main.js
@@ -18,18 +18,18 @@ for (const link of links) {
 }
 
 //add header shadow on scroll
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function () {
   if (this.window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
-});
+}
 
-/* testimonials carousel slider swiper */
+//testimonials carousel slider swiper
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   pagination: {
@@ -39,7 +39,7 @@ const swiper = new Swiper(".swiper", {
   keyboard: true,
 });
 
-/* scroll reveal */
+//scroll reveal
 const scrollReveal = ScrollReveal({
   origin: "top",
   distance: "30px",
@@ -52,7 +52,25 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 );
+
+//Back to top button
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+//When scroll
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
