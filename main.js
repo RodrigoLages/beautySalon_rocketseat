639067def7c4813871ebdb1f33cd0d0e6968c25a
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-29 14:07:08 -0300

    Menu tweak

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 //add header shadow on scroll
-function changeHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
+function changeHeaderWhenScroll() {
   if (this.window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
@@ -65,9 +65,9 @@ scrollReveal.reveal(
 );
 
 //Back to top button
-function backToTop() {
-  const backToTopButton = document.querySelector(".back-to-top");
+const backToTopButton = document.querySelector(".back-to-top");
 
+function backToTop() {
   if (this.window.scrollY >= 560) {
     backToTopButton.classList.add("show");
   } else {
@@ -75,8 +75,35 @@ function backToTop() {
   }
 }
 
+//Change menu each section
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (let section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
 //When scroll
 window.addEventListener("scroll", function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
