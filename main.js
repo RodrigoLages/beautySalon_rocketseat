b9a8aefa3456dc711008804415a25b9ff416e417
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-03-29 11:13:32 -0300

    Responsive (1200px)

--- main.js
+++ main.js
@@ -37,6 +37,12 @@ const swiper = new Swiper(".swiper", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 //scroll reveal
